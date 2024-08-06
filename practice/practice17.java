949afcfd3b7e52ee2419504395c76f5884928d72
unknown <leejaehee0807@catholic.ac.kr>
2024-08-06 09:21:00 +0900

    ㅇ자17, 자연수 뒤집어 배열로 만들기

---
+++ practice/practice17.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class practice17 {
+    public int[] solution(long n) {
+        String a = ""+n;
+        int [] answer = new int[a.length()];
+        int cnt =0;
+        while (n > 0){
+            answer[cnt] = (int)(n%10);
+            n /=10;
+            cnt++;
+        }
+
+
+        return answer;
+    }
+}
